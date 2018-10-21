47507bddcbdaa6e22fb539c52c86279b82e89ac2
vinifig <vinifig@hotmail.com>
2018-10-21 16:25:33 -0300

    Remove logger from production mode

--- src/app/store/builder.js
+++ src/app/store/builder.js
@@ -3,14 +3,15 @@ import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from 'redux-logger';
 import users from '../services/reducers/users';
 
+let middlewares = [thunkMiddleware]
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    middlewares.push(loggerMiddleware);
+}
 const buildStore = () => createStore(
     combineReducers({
         users
     }),
-    applyMiddleware (
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    applyMiddleware (...middlewares)
 );
 
 export default buildStore;
